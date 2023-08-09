2d0fe39f29fbaffe1b790f0988a025be45a16806
Aytaç Kılınç <47327815+mrtoc06@users.noreply.github.com>
2023-08-09 21:37:15 +0300

    Little bit work

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,2 +1,15 @@
 /// <reference types="chrome"/>
 
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.storage.sync.set({ dataset: [] });
+  chrome.contextMenus.create({
+    title: "Form Builder",
+    id: "ctxMenuMain",
+    contexts: ["page", "selection"],
+  });
+  chrome.contextMenus.onClicked.addListener((e) => {
+    chrome.storage.sync.get;
+  });
+});
+
+console.log("Background script is running...");
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import "./popup.css";
 
-/// <reference types="chrome"/>
+import App from "./App";
 
-const test = <img src="icon.png" />;
+/// <reference types="chrome"/>
 
 const app = document.createElement("div");
 app.id = "root";
@@ -13,5 +13,4 @@ document.body.appendChild(app);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(test);
-
+root.render(<App />);
